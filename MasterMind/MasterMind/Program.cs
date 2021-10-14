e199fea56e719abafcc0b6f2d363533c26b96b5a
simonabras <91312929+simonabras@users.noreply.github.com>
2021-10-14 16:00:28 +0200

    Add files via upload

---
+++ MasterMind/MasterMind/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterMind
+{
+    class Program
+    {
+        static void Colors(out int[] colors)
+        {
+            colors = new int[4];
+            for (int i = 0; i <= 3; i++)
+            {
+                Random color = new Random();
+                colors[i] = color.Next(7);
+            }
+        }
+        static void ReadColors(int[] colors)
+        {
+            for (int i = 0; i <= 3; i++)
+            {
+                Console.Write(colors[i]);
+            }
+            Console.Read();
+        }
+        static void Main(string[] args)
+        {
+            Colors(out int[] colors);
+            ReadColors(colors);
+        }
+    }
+}
