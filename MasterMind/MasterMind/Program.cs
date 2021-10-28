fcfb3ede0f477b465002c8f04bfb8232bed60c1f
Abras Simon <190242@CCM-Asty-Moulin.local>
2021-10-28 15:52:41 +0200

    Ajout de manches + Bug des pions blanc résolu

--- MasterMind/MasterMind/Program.cs
+++ MasterMind/MasterMind/Program.cs
@@ -12,7 +12,7 @@ namespace MasterMind
         {
             programColorsArray = new int[4];
             Random number = new Random();
-            for (int i = 0; i <= 3; i++)
+            for (int i = 0; i < 4; i++)
             {
                 // Nombre aléatoire entre 0 et 6
                 programColorsArray[i] = number.Next(7);
@@ -20,13 +20,13 @@ namespace MasterMind
         }
         static void ReadColors(int[] colors)
         {
-            for (int i = 0; i <= 3; i++)
+            for (int i = 0; i < 4; i++)
             {
                 Console.Write(colors[i]);
             }
             Console.Read();
         }
-        static void playerColorsToArray(string playerColors, out int[] playerColorsArray)
+        static void PlayerColorsToArray(string playerColors, out int[] playerColorsArray)
         {
             playerColorsArray = new int[4];
             for (int i = 0; i <= 3; i++)
@@ -42,12 +42,12 @@ namespace MasterMind
             whitePawn = 0;
 
             // Ajout des couleurs de l'ordinateur dans un tableau comparatif
-            for (int i = 0; i <= 3; i++)
+            for (int i = 0; i < 4; i++)
             {
                 programColorsArray_[i] = programColorsArray[i];
             }
             // Ajout des pions rouges
-            for (int i = 0; i <= 3; i++)
+            for (int i = 0; i < 4; i++)
             {
                 if (playerColorsArray[i] == programColorsArray[i])
                 {
@@ -57,37 +57,65 @@ namespace MasterMind
                 }
             }
             // Ajout des pions blancs
-            for (int i = 0; i <= 3; i++)
+            for (int i = 0; i < 4; i++)
             {
-                if(Array.Exists(playerColorsArray, element => element == programColorsArray_[i]))
+                for (int j = 0; j < 4; j++)
                 {
-                    whitePawn++;
+                    if (playerColorsArray[i] == programColorsArray_[j])
+                    {
+                        playerColorsArray[i] = -2;
+                        whitePawn++;
+                    }
                 }
             }
         }
         static void Main(string[] args)
         {
+            int round = 1;
+            int redPawn = 0;
+            int whitePawn = 0;
+
             // Générer les couleurs de l'ordinateur
             GenerateColors(out int[] programColorsArray);
-            // Demander au joueur d'encoder les couleurs
-            Console.WriteLine("-------");
-            Console.WriteLine("Blanc = 0");
-            Console.WriteLine("Noir = 1");
-            Console.WriteLine("Bleu = 2");
-            Console.WriteLine("Rouge = 3");
-            Console.WriteLine("Vert = 4");
-            Console.WriteLine("Jaune = 5");
-            Console.WriteLine("-------");
-            Console.WriteLine("Entrez 4 couleurs de votre choix");
-            // Stocker les couleurs du joueur
-            string playerColors = Console.ReadLine();
-            // Convertir les couleurs du joueur en tableau
-            playerColorsToArray(playerColors, out int[] playerColorsArray);
-            // Comparer les couleurs de l'ordinateur aux couleurs du joueur
-            CompareColors(programColorsArray, playerColorsArray, out int redPawn, out int whitePawn);
-            Console.WriteLine($"Pions rouges : {redPawn}");
-            Console.WriteLine($"Pions blancs : {whitePawn}");
-            Console.ReadLine();
+            programColorsArray[0] = 1;
+            programColorsArray[1] = 2;
+            programColorsArray[2] = 3;
+            programColorsArray[3] = 4;
+            while (redPawn != 4 && round <= 12)
+            {
+                // Demander au joueur d'encoder les couleurs
+                Console.WriteLine($"--- Manche {round} ---");
+                Console.WriteLine("Blanc = 0");
+                Console.WriteLine("Noir = 1");
+                Console.WriteLine("Bleu = 2");
+                Console.WriteLine("Rouge = 3");
+                Console.WriteLine("Vert = 4");
+                Console.WriteLine("Jaune = 5");
+                Console.WriteLine("-------");
+                Console.WriteLine("Entrez 4 couleurs de votre choix");
+                // Stocker les couleurs du joueur
+                string playerColors = Console.ReadLine();
+                // Convertir les couleurs du joueur en tableau
+                PlayerColorsToArray(playerColors, out int[] playerColorsArray);
+                // Comparer les couleurs de l'ordinateur aux couleurs du joueur
+                CompareColors(programColorsArray, playerColorsArray, out redPawn, out whitePawn);
+                Console.WriteLine($"Pions rouges : {redPawn}");
+                Console.WriteLine($"Pions blancs : {whitePawn}");
+                if (redPawn == 4)
+                {
+                    Console.WriteLine("Bien joué ! Vous avez gagné !");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    round++;
+                    if (round > 12)
+                    {
+                        Console.WriteLine("Dommage ! Vous avez perdu !");
+                        Console.ReadLine();
+                    }
+                }
+            }
         }
     }
 }
